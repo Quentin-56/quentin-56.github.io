436b1257a3de81f8c5ae046668937e4bc50c50b8
quentin-56 <kantin56@yahoo.fr>
2020-02-17 17:48:16 +0100

    correction JS

--- js/test.js
+++ js/test.js
@@ -1,7 +1,6 @@
 $(function() {
   $('.tab').on('click', function() {
     $this = $(this);
-    
     // Update the tabs
     $('.tab.current').removeClass('current').attr('aria-selected', false);
     $this.addClass('current').attr('aria-selected', true);
@@ -15,13 +14,6 @@ let _changeTabPanel = panelName => {
   $('.panel.current').removeClass('current').addClass('hidden').attr('aria-hidden', true);
   $('#' + panelName).addClass('current').removeClass('hidden').attr('aria-hidden', false);
 }
-
-
-const btn = document.getElementById("btn");
-btn.addEventListener("click", (e)=>{
-  console.log("mon event : "+e);
-});
-
 
 const counter = document.getElementById("counter");
 
